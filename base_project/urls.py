2345153b56bb098b54026d13a4bae84f2c1a7a4d
Prashanth Selvam <prashanth@simondata.com>
2023-01-21 13:21:43 -0500

    Update urls file

--- base_project/urls.py
+++ base_project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("app/", include("main.urls")),
-    path('', RedirectView.as_view(url='main/', permanent=True)),
+    path('', RedirectView.as_view(url='app/', permanent=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
