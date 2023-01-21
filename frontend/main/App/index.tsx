bb0ff85cb5385ff25e54d663f977d74a7f039ba5
Prashanth Selvam <prashanth@simondata.com>
2023-01-21 12:55:19 -0500

    Added react router support

--- frontend/main/App/index.tsx
+++ frontend/main/App/index.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is an about page</h1>
+    </div>
+  );
+};
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: "app",
+    element: <App />,
+  },
+  {
+    path: "app/about",
+    element: <About />,
+  },
+]);
+
+const AppWithRouter = () => {
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+};
+
+export default AppWithRouter;
