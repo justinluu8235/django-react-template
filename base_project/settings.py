ad7f4698c3876e66dc3d38239909435ff6c43698
Justin Luu <justinluu8235@gmail.com>
2023-03-13 19:31:23 -0700

    add eb to installed apps

--- base_project/settings.py
+++ base_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main.apps.MainConfig",
+    "kitchen-assistant-env.eba-5fukkmch.us-west-2.elasticbeanstalk.com",
 ]
 
 MIDDLEWARE = [
